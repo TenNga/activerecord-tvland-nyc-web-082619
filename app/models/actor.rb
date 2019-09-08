ff464f24570288509872036340be8a035603b778
Kevin Karma <kevinkarma55@gmail.com>
2019-09-08 21:04:39 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,6 @@
 class Actor < ActiveRecord::Base
   has_many :characters
-  has_may :shows, through :characters
+  has_may :shows, through: :characters
   
   def full_name
     "#{self.first_name } #{self.last_name}""
